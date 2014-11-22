49807be14a924d7c3995da6a8ef1c6b1c01e9247
Jan Vcelak <jv@fcelda.cz>
2014-11-23 00:15:49 +0100

    config: template to override timezone

--- config.rb
+++ config.rb
@@ -1,5 +1,7 @@
 require_relative 'lib/app_config'
 
+#ENV["TZ"] = "Europe/Prague"
+
 if ENV.include? 'OPENSHIFT_APP_UUID'
   db_url = ENV["OPENSHIFT_MYSQL_DB_URL"].sub(/^mysql:/, "mysql2:")
   db_url = File.join db_url, ENV["OPENSHIFT_APP_NAME"]
